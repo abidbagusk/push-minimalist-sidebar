ac0884c62198525589990f31e032ff2b4d1e5ec7
abidbagusk <abidbagudkurniawan@gmail.com>
2019-02-02 11:35:07 +0700

    add animation when click search

--- js/main.js
+++ js/main.js
@@ -2,4 +2,10 @@ $(document).ready(function () {
     $('#sidenav__menu').click(function () {
         $('.navigation').toggleClass('toggle-nav');
     })
+    $('#search-button').click(function () {
+        $('.modal-search').addClass('active');
+    });
+    $('.modal-close').click(function () {
+        $('.modal-search').removeClass('active');
+    });
 });
